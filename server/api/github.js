1cbfe98dc8b0dfe479056db28303f6d27977fb46
沈关林 <grinson50@gmail.com>
2022-07-24 21:08:04 +0800

    remove redis temperaly

--- server/api/github.js
+++ server/api/github.js
@@ -5,10 +5,10 @@ const { createCodeHandler } = require('./utils');
 
 module.exports = createCodeHandler(async (code, uuid) => {
   const { CLIENT_ID, CLIENT_SECRET, UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } = process.env;
-  const redis = new Redis({
-    url: UPSTASH_REDIS_REST_URL,
-    token: UPSTASH_REDIS_REST_TOKEN,
-  });
+  // const redis = new Redis({
+  //   url: UPSTASH_REDIS_REST_URL,
+  //   token: UPSTASH_REDIS_REST_TOKEN,
+  // });
 
   console.log('fetching');
 
@@ -36,7 +36,7 @@ module.exports = createCodeHandler(async (code, uuid) => {
     throw new Error(`Cannot resolve response from GitHub`);
   }
 
-  await redis.set(uuid, accessToken);
-  console.log('redis token', await redis.set(uuid));
+  // await redis.set(uuid, accessToken);
+  // console.log('redis token', await redis.set(uuid));
   return accessToken;
 });
