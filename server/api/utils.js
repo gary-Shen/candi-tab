14279c970d8da6848dbf78287e042b313000249c
gary-Shen <grinson50@gmail.com>
2023-06-26 13:38:25 +0800

    feat: new github access token server page

--- server/api/utils.js
+++ server/api/utils.js
@@ -32,8 +32,80 @@ function sendRejection(response, status = 400, content) {
 }
 
 function writeJSON(response, data) {
-  response.setHeader('Content-Type', 'application/json');
-  response.write(JSON.stringify(data));
+  response.setHeader('Content-Type', 'text/html');
+  response.write(`
+  <html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      href="https://unpkg.com/@primer/css@^20.2.4/dist/primer.css"
+      rel="stylesheet"
+    />
+    <title>Github access token for candi-tab</title>
+    <script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.11/dist/clipboard.min.js"></script>
+    <style>
+      #content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      }
+
+      .inputs {
+        display: flex;
+      }
+
+      .inputs > * {
+        padding: 0.5rem 0.6rem;
+        border: 1px solid #ccc;
+        border-radius: 6px;
+      }
+
+      .inputs button {
+        border-top-left-radius: 0;
+        border-bottom-left-radius: 0;
+      }
+
+      .inputs input {
+        border-right: 0;
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+      }
+    </style>
+  </head>
+
+  <body>
+    <div id="content">
+      <h1>Success!</h1>
+      <p>Copy the following access token and paste it into the extension.</p>
+      <div class="inputs">
+        <input
+          readonly
+          class="form-control"
+          type="text"
+          id="token"
+          type="text"
+          value="${data.accessToken}"
+          placeholder="Standard input"
+          aria-label="Repository description"
+        />
+        <button
+          class="btn tooltipped tooltipped-nw"
+          type="button"
+          id="copy"
+          data-clipboard-target="#token"
+          aria-label="This is the tooltip on the North West side."
+        >
+          Copy
+        </button>
+      </div>
+    </div>
+    <script>
+      new ClipboardJS('#copy');
+    </script>
+  </body>
+</html>
+
+  `);
 }
 
 module.exports = {
